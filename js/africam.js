d496478112c75a2fd9e511c838af509e78c3fd4c
Olatunde Sleek <47743182+olatundesleek@users.noreply.github.com>
2019-08-13 18:14:17 +0100

    Update africam.js
    edit to js file

--- js/africam.js
+++ js/africam.js
@@ -1,17 +1,31 @@
-const toggleBtn = document.querySelector('#toggle span');
-const menuOverlay = document.querySelector('#menu-overlay');
+// HAMBURGER
+const toggleBtn_div = document.querySelector('#toggle .hamburger');
+const sideBar_div = document.querySelector('#sidebar-menu');
+
+// LOGIN POP-UP
+const loginForm_div = document.querySelector('.login-form');
 
 document.addEventListener('click', event => {
 	if (
 		event.target.classList.contains('hamburger') ||
 		event.target.classList.contains('line')
 	) {
-		toggleBtn.classList.toggle('toggle');
-		menuOverlay.classList.toggle('toggle');
+		toggleBtn_div.classList.toggle('toggle');
+		sideBar_div.classList.toggle('toggle');
 	} else if (
-		!(event.target === menuOverlay || event.target.id === 'login-signup')
+		!(
+			event.target === sideBar_div ||
+			event.target.id === 'sect-one' ||
+			event.target.id === 'sect-two'
+		)
 	) {
-		menuOverlay.classList.remove('toggle');
-		toggleBtn.classList.remove('toggle');
+		sideBar_div.classList.remove('toggle');
+		toggleBtn_div.classList.remove('toggle');
 	}
+
+	if (event.target.classList.contains('nav-login-btn'))
+		loginForm_div.classList.add('loginPop');
+	else if (event.target.classList.contains('login-form-close'))
+		loginForm_div.classList.remove('loginPop');
 });
+
