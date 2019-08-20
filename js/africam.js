ba3f9f33acb593b7829a2d31b46d750be5e6ed03
olatundesleek <olatundesleek@gmail.com>
2019-08-20 12:56:59 +0100

    major changes made to the html(browser scroll),and css(new styling),and js

--- js/africam.js
+++ js/africam.js
@@ -1,36 +1,36 @@
-// HAMBURGER
-const toggleBtn_div = document.querySelector("#toggle .hamburger");
-const sideBar_div = document.querySelector("#sidebar-menu");
+import * as DomVariables from './components/DomVariables.js';
+const {
+	toggleBtn_div,
+	sideBar_div,
+	dropdownList_li,
+	loginForm_div
+} = DomVariables;
 
-// LOGIN POP-UP
-const loginForm_div = document.querySelector(".login-pop-up");
+document.addEventListener('click', event => {
+	if (event.target === dropdownList_li) {
+		dropdownList_li.classList.toggle('toggle');
+	}
 
-document.addEventListener("click", event => {
-  const dropdownList_li = event.target.parentElement.parentElement;
+	if (
+		event.target.classList.contains('hamburger') ||
+		event.target.classList.contains('line')
+	) {
+		toggleBtn_div.classList.toggle('toggle');
+		sideBar_div.classList.toggle('toggle');
+	} else if (
+		!(
+			event.target === sideBar_div ||
+			event.target.id === 'sect-one' ||
+			event.target.id === 'sect-two' ||
+			event.target === dropdownList_li
+		)
+	) {
+		sideBar_div.classList.remove('toggle');
+		toggleBtn_div.classList.remove('toggle');
+	}
 
-  if (
-    event.target.classList.contains("hamburger") ||
-    event.target.classList.contains("line")
-  ) {
-    toggleBtn_div.classList.toggle("toggle");
-    sideBar_div.classList.toggle("toggle");
-  } else if (
-    !(
-      event.target === sideBar_div ||
-      event.target.id === "sect-one" ||
-      event.target.id === "sect-two" ||
-      dropdownList_li.classList.contains("dropdown-list")
-    )
-  ) {
-    sideBar_div.classList.remove("toggle");
-    toggleBtn_div.classList.remove("toggle");
-  }
-
-  if (event.target.classList.contains("nav-login-btn"))
-    loginForm_div.classList.add("loginPop");
-  else if (event.target.classList.contains("login-form-close"))
-    loginForm_div.classList.remove("loginPop");
-
-  if (dropdownList_li.classList.contains("dropdown-list"))
-    dropdownList_li.classList.toggle("toggle");
+	if (event.target.classList.contains('nav-login-btn'))
+		loginForm_div.classList.add('loginPop');
+	else if (event.target.classList.contains('login-form-close'))
+		loginForm_div.classList.remove('loginPop');
 });
---
+++ js/components/DomVariables.js
@@ -0,0 +1,9 @@
+// HAMBURGER
+export const toggleBtn_div = document.querySelector('#toggle .hamburger');
+export const sideBar_div = document.querySelector('#sidebar-menu');
+
+// DROPDOWN_LIST
+export const dropdownList_li = document.querySelector('.dropdown-list');
+
+// LOGIN POP-UP
+export const loginForm_div = document.querySelector('.login-form-pop-up');
